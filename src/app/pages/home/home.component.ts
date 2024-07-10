5b7a2117769bf7c06567c0235bbdf9de086f20be
devrrodrigues <rrodrigues.dev@outlook.com>
2024-07-10 15:39:21 -0300

    correcao

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,12 +4,10 @@ import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
   faWhatsapp = faWhatsapp;
   videoIds: any;
-
-
 }
