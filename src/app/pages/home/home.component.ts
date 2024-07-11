51f3b8e1f1c208de8534cad61886885b8f7227a4
devrrodrigues <rrodrigues.dev@outlook.com>
2024-07-11 11:10:53 -0300

    RESOLVIDO PROBLEMA DE AUTO CARREGAR VIDEO

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
-import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
 
-  faWhatsapp = faWhatsapp;
-  videoIds: any;
+  @ViewChild('voleiVideo') videoElement!: ElementRef<HTMLVideoElement>;
+
+  ngAfterViewInit(): void {
+    const video: HTMLVideoElement = this.videoElement.nativeElement;
+
+    video.setAttribute('autoplay', '');
+    video.muted = true; // Certifique-se de que o vídeo está silenciado
+
+    const tryPlayVideo = () => {
+      console.log('Tentando reproduzir o vídeo...');
+      video.play().then(() => {
+        console.log('Vídeo está reproduzindo.');
+      }).catch(error => {
+        console.error('A reprodução automática falhou:', error);
+      });
+    };
+
+    video.addEventListener('canplay', () => {
+      console.log('Evento canplay disparado');
+      tryPlayVideo();
+    });
+
+    video.addEventListener('loadeddata', () => {
+      console.log('Evento loadeddata disparado');
+      tryPlayVideo();
+    });
+
+    // Tentar reproduzir o vídeo imediatamente
+    tryPlayVideo();
+  }
 }
